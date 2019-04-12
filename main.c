e41123c34695988ae2fe0b621b00f07d185ceea8
yewewew <21800667@handong.edu>
2019-04-12 10:59:13 +0900

    정예원LAB PR 보냅니다

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void sub1(){
+  printf("안녕하세요, 18학번 전산전자공학부 정예원이라고 합니다:)");
+}
+void sub2();
+void sub3();
+void sub4();
+
+int main(){
+  sub1();
+  sub2();
+  sub3();
+  sub4();
+  return 0;
+}
